7bd1b203cff6e0d61adaabf5b903b4843bf47410
srrod92 <66223450+srrod92@users.noreply.github.com>
2026-09-14 13:13:30 -0300

    Clase 5 completa

---
+++ Clase5/CSharp EstructurasDeDatosUPSO/EstructurasDeDatosUPSO/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Http.Headers;
+
+namespace EstructurasDeDatosUPSO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> mochila = new Dictionary<string, int>();
+
+            mochila.Add("Espada", 1);
+            mochila.Add("Pocion de vida", 2);
+            mochila.Add("Escudo", 1);
+
+            ImprimirMochila(mochila);
+
+            UsarItem(mochila, "Escudo");
+            UsarItem(mochila, "Pocion de vida");
+
+            ImprimirMochila(mochila);
+
+
+            string cadena = "Esto es un array";
+            char[] cadenaParseada = cadena.ToCharArray();
+            cadenaParseada[11] = 'B';
+            cadena = new string(cadenaParseada);
+
+            Console.WriteLine(cadena);
+
+
+
+            string[,] matriz = new string[10,10];
+
+            for (int x = 0; x < matriz.GetLength(0); x++)
+            {
+                for (int y = 0; y < matriz.GetLength(1); y++)
+                {
+                    matriz[x, y] = $"({x},{y})";  // (0,0)  (0,1)  (0,2) ....   (1,0)
+                }
+            }
+
+            for (int x = 0; x < matriz.GetLength(0); x++)
+            {
+                for (int y = 0;y < matriz.GetLength(1); y++)
+                {                   
+                    Console.Write(matriz[x,y]);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void ImprimirMochila(Dictionary<string, int> mochila)
+        {
+            Console.WriteLine("Inventario: ");
+            foreach (var par in mochila)
+            {
+                Console.WriteLine($"{par.Key}: {par.Value.ToString()}");
+            }
+        }
+
+        static void UsarItem(Dictionary<string, int> mochila, string item)
+        {
+
+            if (mochila.ContainsKey(item))
+            {
+                Console.WriteLine($"Usando un {item}");
+                
+                Console.WriteLine($"{item} restantes: {--mochila[item]}");
+
+                if (mochila[item] <= 0)
+                {
+                    mochila.Remove(item);
+                }
+            }
+        }
+
+    }
+}
---
+++ Clase5/CSharp EstructurasDeDatosUPSO/GuiaMatrices/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace GuiaMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mapa = CrearMatriz(10, 12);         
+
+            ImprimirMatriz(mapa);
+
+            Tuple<int, int> coordenadas = TomarCoordenadas();
+
+            CambiarTerreno(mapa, coordenadas);
+
+            ImprimirMatriz(mapa);
+
+        }
+        static void ImprimirMatriz(int[,] matriz)
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write($"{i}. ");
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"| {matriz[i, j]} |");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.Write(" ");
+            for (int i = 0;i < matriz.GetLength(1); i++)
+            {
+                Console.Write($"{i,5}");
+            }
+            Console.WriteLine();
+        }
+
+        static int[,] CrearMatriz(int ancho, int alto)
+        {
+            int[,] mapa = new int[ancho, alto];
+
+            for(int i = 0; i<ancho; i++)
+            {
+                for (int j =0; j<ancho; j++)
+                {
+                    mapa[i,j] = Random.Shared.Next(2);
+                }
+            }
+
+            return mapa;
+        }
+
+
+        static Tuple<int,int> TomarCoordenadas()
+        {
+            Console.WriteLine("Ingrese coordenada X para modificar: ");
+            int coordenadaX = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese coordenada Y para modificar: ");
+            int coordenadaY = int.Parse(Console.ReadLine());
+
+            return new Tuple<int,int>(coordenadaX, coordenadaY);
+        }
+
+
+        static void CambiarTerreno(int[,] mapa, Tuple<int,int> cambiar)
+        {
+            mapa[cambiar.Item1, cambiar.Item2] = mapa[cambiar.Item1, cambiar.Item2] == 0 ? 1 : 0;
+        }
+
+
+    }
+
+
+}
